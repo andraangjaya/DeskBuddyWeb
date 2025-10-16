acd2d32a1ffeb0b73f18997a94f34006d145c384
andra <andrabaktiangjaya@gmail.com>
2025-10-16 15:16:05 +0800

    add calculate dynamically for total session, members, and active now

--- src/app/sesison-history/session-history-update/session-history-update.component.ts
+++ src/app/sesison-history/session-history-update/session-history-update.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit, signal} from '@angular/core';
+import {Component, computed, inject, OnInit, signal} from '@angular/core';
 import {SidebarComponent} from '../../component/sidebar/sidebar.component';
 import {SessionHistoryService} from '../session-history.service';
 import {SessionHistory} from '../session-history.model';
@@ -17,6 +17,17 @@ import {DatePipe} from '@angular/common';
 export class SessionHistoryUpdateComponent implements OnInit{
   sessionHistories = signal<SessionHistory[]>([]);
   private readonly sessionHistoryService = inject(SessionHistoryService);
+  totalMembers = computed(() => {
+    const data = this.sessionHistories();
+    const unique = new Set(
+      data.map(s => `${s.firstName?.trim().toLowerCase()} ${s.lastName?.trim().toLowerCase()}`)
+    );
+    return unique.size;
+  });
+
+  activeNow = computed(() =>
+    this.sessionHistories().filter(s => s.status?.toUpperCase() === 'IN_PROGRESS').length
+  );
 
     ngOnInit(): void {
         this.sessionHistoryService.query().subscribe(res =>{
